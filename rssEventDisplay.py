94abe30fcc7013e94a78aacff4cfe95ebbaa1543
jdogmcsteezy <jesse.sasquatch.schmidt@gmail.com>
2018-01-25 20:09:17 -0800

    Remove testing from rssEventDisplay and put testing in its own file

--- rssEventDisplay.py
+++ rssEventDisplay.py
@@ -82,30 +82,3 @@ class AcademicCalanderDisplay(Surface):
         return self
 
 
-# ---- TESTING -----
-import pygame
-def main():
-    pygame.init()
-    clock = pygame.time.Clock()
-    testScreen = pygame.display.set_mode((1200, 75))
-    testScreen.fill((200, 255, 230))
-    ticker = AcademicCalanderDisplay(1200, 75)
-    testScreen.blit(ticker, (0, 0))
-    pygame.display.update()
-    run = True
-    while(run):
-        clock.tick(26)
-        testScreen.blit(ticker.Update(), (0, 0))
-        pygame.display.update()
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == ord('q'):
-                    run = False
-            elif event.type == pygame.QUIT:
-                run = False
-
-    pygame.quit()
-
-
-if __name__ == "__main__":
-    main()
